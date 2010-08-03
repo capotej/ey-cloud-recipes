ab73951e9c5d4cb6ac119036fc90bf0d95f54256
Julio Capote <jcapote@gmail.com>
2010-08-02 20:01:51 -0700

    stuff

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 remote_file "/tmp/redis2.tar.gz" do
-  source "http://redis.googlecode.com/files/redis-2.0.0-rc3.tar.gz"
+  source "http://redis.googlecode.com/files/redis-2.0.0-rc4.tar.gz"
   mode "0644"
   action :create_if_missing
 end
@@ -16,13 +16,13 @@ bash "untar-redis" do
 end
 
 bash "compile-redis" do
-  code "(cd /tmp/redis-2.0.0-rc3; make)"
+  code "(cd /tmp/redis-2.0.0-rc4; make)"
 end
 
 files = %w{ redis-server redis-benchmark redis-cli redis-check-dump redis-check-aof }
 files.each do |f|
   bash "install-#{f}" do
-    code "mv /tmp/redis-2.0.0-rc3/#{f} /usr/local/bin/#{f}"
+    code "mv /tmp/redis-2.0.0-rc4/#{f} /usr/bin/#{f}"
   end
 end
 
@@ -33,19 +33,19 @@ directory "/data/redis" do
   recursive true
 end
 
-template "/etc/redis_util.conf" do
+template "/etc/redis/redis.conf" do
   owner 'root'
   group 'root'
   mode 0644
   source "redis.conf.erb"
   variables({
-    :pidfile => '/var/run/redis_util.pid',
-    :basedir => '/data/redis',
-    :logfile => '/data/redis/redis.log',
-    :port  => '6380',
-    :loglevel => 'notice',
-    :timeout => 300000,
-  })
+              :pidfile => '/var/run/redis_util.pid',
+              :basedir => '/data/redis',
+              :logfile => '/data/redis/redis.log',
+              :port  => '6380',
+              :loglevel => 'notice',
+              :timeout => 300000,
+  }) 
 end
 
 template "/data/monit.d/redis_util.monitrc" do
@@ -54,11 +54,11 @@ template "/data/monit.d/redis_util.monitrc" do
   mode 0644
   source "redis.monitrc.erb"
   variables({
-    :profile => '1',
+              :profile => '1',
               :configfile => '/etc/redis_util.conf',
-    :pidfile => '/var/run/redis_util.pid',
-    :logfile => '/data/redis',
-    :port => '6380',
+              :pidfile => '/var/run/redis_util.pid',
+              :logfile => '/data/redis',
+              :port => '6380',
   })
 end
 
